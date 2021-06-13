3e51e7f5208b6e38d132392cc687bf192c74c6d9
Juan Carlos Camussi <juanca@caylent.com>
2021-06-13 19:27:33 -0300

    testo

--- hello/handler.py
+++ hello/handler.py
@@ -9,13 +9,13 @@ def hello(event, context):
 
     response = {"statusCode": 200, "body": json.dumps(body)}
 
-    return response
+    #return response
 
     # Use this code if you don't use the http event with the LAMBDA-PROXY
     # integration
-    """
+    
     return {
         "message": "Go Serverless v1.0! Your function executed successfully!",
         "event": event
     }
-    """
+    
